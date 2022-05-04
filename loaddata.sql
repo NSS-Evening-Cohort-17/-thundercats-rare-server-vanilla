573891cc2623c50ba1f31afa2fe7cab0c16a48a9
Steve Nagel <stevencnagel@gmail.com>
2022-05-04 06:29:24 +0500

    Added initial tag code

--- loaddata.sql
+++ loaddata.sql
@@ -39,7 +39,7 @@ CREATE TABLE "Posts" (
   "image_url" varchar,
   "content" varchar,
   "approved" bit,
-  FOREIGN KEY(`user_id`) REFERENCES `Users`(`id`),
+  FOREIGN KEY(`user_id`) REFERENCES `Users`(`id`)
 );
 
 CREATE TABLE "Comments" (
@@ -88,3 +88,5 @@ CREATE TABLE "Categories" (
 INSERT INTO Categories ('label') VALUES ('News');
 INSERT INTO Tags ('label') VALUES ('JavaScript');
 INSERT INTO Reactions ('label', 'image_url') VALUES ('happy', 'https://pngtree.com/so/happy');
+INSERT INTO Tags ('label') VALUES ('Python');
+INSERT INTO PostTags ('post_id', 'tag_id') VALUES (1, 1);
