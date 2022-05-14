e6d9edb4fa0c333712c9a59b1867b2264cad4f03
Steve Nagel <stevencnagel@gmail.com>
2022-05-14 11:49:12 -0500

    added tag edit and delete func

--- views/tag_requests.py
+++ views/tag_requests.py
@@ -75,3 +75,49 @@ def create_tag(new_tag):
         new_tag["id"] = id
         
     return json.dumps(new_tag)
+
+def update_tag(id, new_tag):
+    with sqlite3.connect("./db.sqlite3") as conn:
+        db_cursor = conn.cursor()
+        
+        db_cursor.execute("""
+        UPDATE Tags
+            SET 
+                label = ?
+        WHERE id = ?
+        """, (new_tag["label"], id))
+        
+        rows_affected = db_cursor.rowcount
+        
+    if rows_affected == 0:
+        return False
+    else:
+        return True
+    
+def do_PUT(self):
+    content_len = int(self.headers.get('content-length', 0))
+    post_body = self.rfile.read(content_len)
+    post_body = json.loads(post_body)
+    
+    (resource, id) = self.parse_url(self.path)
+    
+    success = False
+    
+    if resource == "tags":
+        success = update_tag(id, post_body)
+        
+    if success:
+        self._set_headers(204)
+    else:
+        self._set_headers(404)
+        
+    self.wfile.write("".encode())
+    
+def delete_tag(id):
+    with sqlite3.connect("./db.sqlite3") as conn:
+        db_cursor = conn.cursor()
+        
+        db_cursor.execute("""
+        DELETE FROM Tags
+        WHERE id = ?
+        """, (id, ))
